30074d22bfb77dc812c40c1e6a63283939ed9983
Eugene Kim <eugenekim@cpp.edu>
2022-10-23 18:13:24 -0700

    part 1 done and running

--- loshu.c
+++ loshu.c
@@ -19,21 +19,100 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+const int SIZE = 3;
 
 // Function prototypes
+int test(int arr[3][3]);
 
 int main()
 {
 
+    int result; 
+
     // Create two 2-d array to be tested, one lo shu, one no lo shu
+    int loShu[3][3] = {
+        {4,9,2},
+        {3,5,7},
+        {8,1,6}
+    };
+
+    int noLoShu[3][3] = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
 
     // send lo shu to test function, and print result 
-
+    result = test(loShu);
+    if(result == 1)
+    {
+        printf("First array is Lo Shu Magic Square\n");
+    }
+    else{
+        printf("First array is NOT Lo Shu Magic Square\n");
+    }
 
     // send no lo shu to test function, and print result
-
+    result = test(noLoShu);
+    if(result == 1)
+    {
+        printf("Second array is Lo Shu Magic Square\n");
+    }
+    else
+    {
+        printf("Second array is NOT Lo Shu Magic Square\n");
+    }
 
 
     
     return EXIT_SUCCESS;
+}
+
+
+
+int test(int arr[3][3])
+{
+    
+    // total 8 sum needed
+
+    // find the magic number
+    int target = (arr[0][0] + arr[0][1] + arr[0][2]);
+
+    // row check
+    if((arr[1][0] + arr[1][1] + arr[1][2]) != target)
+    {
+        return 0;
+    }
+    else if((arr[2][0] + arr[2][1] + arr[2][2]) != target)
+    {
+        return 0;
+    }
+    else if((arr[0][0] + arr[1][0] + arr[2][0]) != target)
+    {
+        // col check 
+        return 0;
+    }
+    else if((arr[0][1] + arr[1][1] + arr[2][1]) != target)
+    {
+        // col check 
+        return 0;
+    }
+    else if((arr[0][2] + arr[1][2] + arr[2][2]) != target)
+    {
+        // col check 
+        return 0;
+    }
+    else if((arr[0][0] + arr[1][1] + arr[2][2]) != target)
+    {
+        // diag
+        return 0;
+    }
+    else if((arr[0][2] + arr[1][1] + arr[2][0]) != target)
+    {
+        // diag
+        return 0;
+    }
+    else{
+        return 1;
+    }
 }
