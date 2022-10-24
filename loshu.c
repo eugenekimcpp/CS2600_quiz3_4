13e06f5d4a0a2a586d74bc868c97dd31a648101b
Eugene Kim <eugenekim@cpp.edu>
2022-10-23 17:38:01 -0700

    Init commit

---
+++ loshu.c
@@ -0,0 +1,39 @@
+/*
+    Program: loshu.c
+
+    This program simulate a magic square using a 2-d array. 
+    Write a function that accepts a 2-d array as argument and determine
+    whether the array is lo shu square 
+
+    1) Simple: define two of 2-d array, one lo shu, one fore not.
+       then, send it to test funciton. array define in main
+
+    2) challenging: create and populate 2-d with random # 1-9.
+       No repeat number. then cehck if it is lo shu. 
+       put random number square until is lo shu
+       print 2 thing
+       * total number of square generated and tested before success, the count
+       * print the lo shu 
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// Function prototypes
+
+int main()
+{
+
+    // Create two 2-d array to be tested, one lo shu, one no lo shu
+
+    // send lo shu to test function, and print result 
+
+
+    // send no lo shu to test function, and print result
+
+
+
+    
+    return EXIT_SUCCESS;
+}
